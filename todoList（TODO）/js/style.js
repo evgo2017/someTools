a894ed60683d96d9646fcfbf6cb19e3b88e61830
evenIfAlsoGo <2624095175@qq.com>
2018-01-02 22:36:25 +0800

    change

---
+++ todoList（TODO）/js/style.js
@@ -0,0 +1,48 @@
+new Vue({
+	el: '#todoApp',
+	data: {
+		newTodoText: '',
+		showAchieves: false,
+		showHelp: false,
+		showTrash: false,
+		todos: [
+			{ title: '测试数据 001' },
+			{ title: '测试数据 002' },
+			{ title: '测试数据 003' }
+		],
+		achieves: [],
+		trash: []
+	},
+	methods: {
+		addNewTodo: function () {
+			this.todos.push({ title: this.newTodoText });
+		},
+		showData: function (value) {
+			if(value=='showAchieves') this.showAchieves = !this.showAchieves;
+			else if(value=='showHelp') this.showHelp = !this.showHelp;
+			else if(value=='showTrash') this.showTrash = !this.showTrash;
+		},
+		achieve: function (index, value) {
+			this.todos.splice(index, 1);
+			this.$set(this.achieves, this.achieves.length ,{ title: value });
+		},
+		achieveAll : function () {
+			this.achieves = this.achieves.concat(this.todos);
+			this.todos= [];
+		},
+		restore: function (index, value) {
+			this.achieves.splice(index, 1);
+			this.$set(this.todos, this.todos.length ,{ title: value });
+		},
+		abandon: function (index, value, arr) {
+			if(arr=='todos') this.todos.splice(index, 1);
+			else if(arr=='achieves') this.achieves.splice(index, 1);
+			this.$set(this.trash, this.trash.length ,{ title: value });
+		},
+		/*对所有元素操作*/
+		deleteAll : function () {
+			this.trash = [];
+		}
+		
+	}
+})
